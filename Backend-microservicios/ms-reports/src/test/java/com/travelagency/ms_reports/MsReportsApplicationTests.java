f1fe1b7c50fe7005c33957521177b5994090fb8a
Diego Vega <diego.vega.f@usach.cl>
2026-07-26 23:18:12 -0400

    ms up

---
+++ Backend-microservicios/ms-reports/src/test/java/com/travelagency/ms_reports/MsReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelagency.ms_reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
